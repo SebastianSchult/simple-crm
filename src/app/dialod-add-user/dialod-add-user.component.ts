43ee828f94b95bc549b8af9f3b6b18d7433967ae
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-24 09:20:30 +0100

    implement table user

--- src/app/dialod-add-user/dialod-add-user.component.ts
+++ src/app/dialod-add-user/dialod-add-user.component.ts
@@ -2,30 +2,49 @@ import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
-import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
 import { AsyncPipe } from '@angular/common';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
 import { NgIf } from '@angular/common';
-
-
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dialod-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule, MatDatepickerModule, MatNativeDateModule, FormsModule, AsyncPipe, MatProgressBarModule, CommonModule, NgIf], 
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    AsyncPipe,
+    MatProgressBarModule,
+    CommonModule,
+    NgIf,
+    MatCardModule,
+  ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './dialod-add-user.component.html',
-  styleUrl: './dialod-add-user.component.scss'
+  styleUrl: './dialod-add-user.component.scss',
 })
 export class DialodAddUserComponent {
   firestore: Firestore = inject(Firestore);
@@ -33,12 +52,11 @@ export class DialodAddUserComponent {
   user = new User();
   birthDate: Date | any;
   loading = false;
-  
-  constructor() { 
-    const aCollection = collection(this.firestore, 'items')
+
+  constructor(public dialogRef: MatDialogRef<DialodAddUserComponent>) {
+    const aCollection = collection(this.firestore, 'items');
     this.items$ = collectionData(aCollection);
   }
-  
 
   async saveUser() {
     this.user.birthDate = this.birthDate.getTime();
@@ -50,10 +68,10 @@ export class DialodAddUserComponent {
       const result = await addDoc(usersCollection, { ...this.user });
       console.log('User successfully saved:', result);
       this.loading = false;
+      this.dialogRef.close();
     } catch (error) {
       console.error('Error saving user:', error);
       this.loading = false;
     }
   }
-
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatDialogRef} from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { DialodAddUserComponent } from '../dialod-add-user/dialod-add-user.component';
 import { User } from '../../models/user.class';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 
 @Component({
@@ -14,21 +17,14 @@ import { User } from '../../models/user.class';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
-  imports: [MatButtonModule, MatIcon, MatTooltipModule]
+  imports: [MatButtonModule, MatCardModule, MatIcon, MatTooltipModule, NgFor],
 })
-
 export class UserComponent {
-
   user = new User();
-  
 
-  constructor(public dialog: MatDialog) {
-
-   }
-
+  constructor(public dialog: MatDialog) {}
 
   openDialog() {
     this.dialog.open(DialodAddUserComponent);
   }
-
 }
