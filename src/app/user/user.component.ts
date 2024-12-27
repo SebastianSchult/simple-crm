e9e78f5223db8a7d82d1012ae5315352e32c3648
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-27 13:01:47 +0100

    live update firestore

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { DialodAddUserComponent } from '../dialod-add-user/dialod-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
-import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
-import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { CommonModule, NgFor } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  imports: [MatButtonModule, MatCardModule, MatIcon, MatTooltipModule, NgFor],
+  imports: [MatButtonModule, MatCardModule, MatIcon, MatTooltipModule, NgFor, CommonModule],
 })
-export class UserComponent implements OnInit {
-  users: any[] = []; // Array to hold the users
+export class UserComponent {
+  user = new User();
+  users$: Observable<any[]> = new Observable();
 
   constructor(public dialog: MatDialog, private firestore: Firestore) {}
 
   ngOnInit() {
-    this.loadUsers();  // Call the function to load users from Firestore
+    // Live-Update der User-Daten
+    this.loadUsers();
   }
 
-  // Asynchronous function to load users from Firestore
-  async loadUsers() {
-    const usersCollection = collection(this.firestore, 'users');
-    const userSnapshot = await getDocs(usersCollection);
-    this.users = userSnapshot.docs.map(doc => doc.data());
-    console.log(this.users);  // To check the fetched data
+  loadUsers() {
+    // Referenz auf die Collection 'users' erstellen
+    const userCollection = collection(this.firestore, 'users');
+    
+    // Live-Daten-Stream mit collectionData abonnieren
+    this.users$ = collectionData(userCollection, { idField: 'id' });
+
+    // Optionale Debug-Ausgabe
+    this.users$.subscribe((users) => {
+      console.log('Aktualisierte Benutzerdaten:', users);
+    });
   }
 
   openDialog() {
