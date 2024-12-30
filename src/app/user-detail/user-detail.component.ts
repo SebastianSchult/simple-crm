7359e4f768e26feb7e8511db4115d4f87187c730
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-12-30 13:00:04 +0100

    catch userId at userdetails

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { Firestore, collection, doc, docData, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,6 +12,25 @@ import {MatCardModule} from '@angular/material/card';
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  firestore: Firestore = inject(Firestore);
+  route: ActivatedRoute = inject(ActivatedRoute);  // Injiziere ActivatedRoute
+  user$: Observable<any> | undefined;
+  userID ='';
 
+  ngOnInit() {
+    // Extrahiere die userId aus der URL
+    const userId = this.route.snapshot.paramMap.get('id');
+  
+
+    if (userId) {
+      console.log('Abgerufene User ID:', userId);
+      // Hol das User-Dokument aus Firestore mit der User-ID
+      const userDoc = doc(this.firestore, `users/${userId}`);
+      this.user$ = docData(userDoc);  // Beobachte die Daten des Users
+      
+    }
+  }
 }
+
+
