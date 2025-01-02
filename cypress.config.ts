a61182e3680ffb6b67577fa89cfc7270bd17337a
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2025-01-02 13:14:39 +0100

    bugfixing user details component

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: 'vb2u55',
+  
+  e2e: {
+    'baseUrl': 'http://localhost:4200'
+  },
+  
+  
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts'
+  }
+  
+})
